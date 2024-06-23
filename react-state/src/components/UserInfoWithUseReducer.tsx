1a3dce346af4951c1890e84f13f8d3a15dd9f491
programarreza <programarreza@gmail.com>
2024-06-23 23:01:28 +0600

    added a user info form use Reducer

--- react-state/src/components/UserInfoWithUseReducer.tsx
+++ react-state/src/components/UserInfoWithUseReducer.tsx
@@ -1,11 +1,46 @@
+import { ChangeEvent, useReducer } from "react";
+
+type TAction = {
+  type: string;
+  payload: string;
+};
+
+const initialState = {
+  name: "",
+  age: "",
+  hobbies: [] as string[],
+};
+
+const reducer = (currentState: typeof initialState, action: TAction) => {
+
+  switch (action.type) {
+    case "addName":
+      return { ...currentState, name: action.payload };
+    case "addAge":
+      return { ...currentState, age: action.payload };
+    case "addHobby":
+      return {
+        ...currentState,
+        hobbies: [...currentState.hobbies, action.payload],
+      };
+
+    default:
+      return currentState;
+  }
+};
+
 const UserInfoWithUseReducer = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
+  const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(state);
+  };
 
-
-	
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
+        onChange={(e) => dispatch({ type: "addName", payload: e.target.value })}
         className="border"
         type="text"
         name="name"
@@ -13,6 +48,7 @@ const UserInfoWithUseReducer = () => {
         placeholder="name"
       />
       <input
+        onChange={(e) => dispatch({ type: "addAge", payload: e.target.value })}
         className="border"
         type="number"
         name="age"
@@ -20,6 +56,7 @@ const UserInfoWithUseReducer = () => {
         placeholder="age"
       />
       <input
+        onBlur={(e) => dispatch({ type: "addHobby", payload: e.target.value })}
         className="border"
         type="text"
         name="hobbies"
