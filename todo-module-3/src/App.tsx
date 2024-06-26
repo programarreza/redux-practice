b3ca3bcaaab00261fe383ada7681bde5850503e9
programarreza <programarreza@gmail.com>
2024-06-26 23:49:25 +0600

    todo app project initial setup done

---
+++ todo-module-3/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="m-12">
+      <Button>My button</Button>
+    </div>
+  );
+};
+
+export default App;
