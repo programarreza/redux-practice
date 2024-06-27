19d9747d89dff7ecc5c586d3a22ea1400faee7e1
programarreza <programarreza@gmail.com>
2024-06-27 21:32:16 +0600

    todo add , remove & isComplete status update complete

--- todo-module-3/src/components/ui/todo/AddTodoModal.tsx
+++ todo-module-3/src/components/ui/todo/AddTodoModal.tsx
@@ -1,17 +1,37 @@
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { addTodo } from "@/redux/features/todoSlice";
+import { useAppDispatch } from "@/redux/hook";
+import { FormEvent, useState } from "react";
 import { Button } from "../button";
-import { Label } from "../label";
 import { Input } from "../input";
+import { Label } from "../label";
 
 const AddTodoModal = () => {
+  const [task, setTask] = useState("");
+  const [description, setDescription] = useState("");
+  const dispatch = useAppDispatch();
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const randomString = Math.random().toString(36).substring(2, 7);
+    const taskDetails = {
+      id: randomString,
+      title: task,
+      description: description,
+    };
+
+    console.log(taskDetails);
+
+    dispatch(addTodo(taskDetails));
+  };
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -21,36 +41,40 @@ const AddTodoModal = () => {
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle>Add Task</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Add your tasks that you want to finish.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
+        <form onSubmit={onSubmit} className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
+            <Label htmlFor="task" className="text-right">
+              Task
             </Label>
             <Input
-              id="name"
-              defaultValue="Pedro Duarte"
-              className="col-span-3"
+              onBlur={(e) => setTask(e.target.value)}
+              id="task"
+              placeholder="task"
+              className="col-span-3 border"
             />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
+            <Label htmlFor="description" className="text-right">
+              Description
             </Label>
             <Input
-              id="username"
-              defaultValue="@peduarte"
-              className="col-span-3"
+              onBlur={(e) => setDescription(e.target.value)}
+              id="description"
+              placeholder="description"
+              className="col-span-3 border"
             />
           </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
+          <div className="flex justify-end">
+            <DialogClose asChild>
+              <Button type="submit">Save changes</Button>
+            </DialogClose>
+          </div>
+        </form>
       </DialogContent>
     </Dialog>
   );
--- todo-module-3/src/components/ui/todo/TodoContainer.tsx
+++ todo-module-3/src/components/ui/todo/TodoContainer.tsx
@@ -1,8 +1,11 @@
+import { useAppSelector } from "@/redux/hook";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
 
 const TodoContainer = () => {
+  const { todos } = useAppSelector((state) => state.todos);
+
   return (
     <div>
       <div className="flex justify-between mb-5">
@@ -12,10 +15,9 @@ const TodoContainer = () => {
 
       <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px] ">
         <div className="bg-white p-5 w-full h-full rounded-lg space-y-3 ">
-          <TodoCard />
-          <TodoCard />
-          <TodoCard />
-          <TodoCard />
+          {todos.map((item) => (
+            <TodoCard {...item} />
+          ))}
         </div>
 
         {/* <div className="bg-white text-2xl font-semibold p-3 rounded-md flex justify-center items-center">
