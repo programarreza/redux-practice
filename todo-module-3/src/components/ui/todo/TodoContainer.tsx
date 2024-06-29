e37a5fc9c020283d4893e55744669705fc44c7f1
programarreza <programarreza@gmail.com>
2024-06-29 21:43:37 +0600

    get all task from server use query

--- todo-module-3/src/components/ui/todo/TodoContainer.tsx
+++ todo-module-3/src/components/ui/todo/TodoContainer.tsx
@@ -1,10 +1,19 @@
-import { useAppSelector } from "@/redux/hook";
+import { useGetTodosQuery } from "@/redux/api/api";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
 
 const TodoContainer = () => {
-  const { todos } = useAppSelector((state) => state.todos);
+  // from Local State
+  // const { todos } = useAppSelector((state) => state.todos);
+
+  // from server
+  const { data: todos, isError, isLoading } = useGetTodosQuery(undefined);
+  console.log(todos?.data);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div>
@@ -15,7 +24,7 @@ const TodoContainer = () => {
 
       <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px] ">
         <div className="bg-white p-5 w-full h-full rounded-lg space-y-3 ">
-          {todos.map((item) => (
+          {todos?.data?.map((item) => (
             <TodoCard {...item} />
           ))}
         </div>
